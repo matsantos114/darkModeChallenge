9efee14187a3f74cda8e049c2c2888eb35800bcf
matsantos114 <matsantos114@gmail.com>
2021-10-18 19:31:17 -0600

    Dark mode assignment

---
+++ script.js
@@ -0,0 +1,89 @@
+
+const storeItems = [
+    {
+        name: 'TV',
+        price: 800.00,
+        details: '4K Ultra HD',
+        inStock: true
+        
+    },
+    {
+        name: 'Phone',
+        price: 700.00,
+        inStock: false,
+        details: '5G'
+    },
+    {
+        name: 'Game Console',
+        price: 300.00,
+        inStock: true,
+        details: 'The latest and greatest'
+    },
+    {
+        name: 'Laptop',
+        price: 1200.00,
+        inStock: true,
+        details: '16GB RAM 1TB SSD'
+    },
+    {
+        name: 'Smart Watch',
+        price: 200.00,
+        inStock: false,
+        details: 'Counts your steps'
+    },
+    {
+        name: 'Headphones',
+        price: 100.00,
+        inStock: true,
+        details: 'Clearest music to be heard'
+    },
+    {
+        name: 'Keyboard',
+        price: 100.00,
+        inStock: true,
+        details: 'Types for you'
+    },
+    {
+        name: 'HDMI Cord',
+        price: 100.00,
+        inStock: true,
+        details: 'HDMI to USB type C'
+    },
+    {
+        name: 'Monitor',
+        price: 300.00,
+        inStock: true,
+        details: '4K Ultra HD'
+    },
+    {
+        name: 'Speaker',
+        price: 200.00,
+        inStock: true,
+        details: 'Clearest music to be heard'
+    },
+    {
+        name: 'Video Game',
+        price: 60.00,
+        inStock: true,
+        details: 'Enjoy for hours'
+    },
+];
+
+
+storeItems.forEach(function (item){
+    console.log(item)
+    if(item.inStock === true) {
+        $("#products").append(`
+        <div class='inStock'>
+            <div>${item.name}</div> 
+            <div>$${item.price}</div> 
+            <div>${item.details}</div>
+        </div>`)
+
+      }
+})
+
+$('#clickMe').on('click', () => {
+    $("#contentContainer").toggleClass('darkMode')
+   
+})
